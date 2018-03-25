c861dc8eed5cc253f4d87619f00825f8fdd24f2d
daniel.moreno <daniel.moreno@accenture.com>
2018-03-25 23:46:11 +0200

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 // Requires
 var express = require('express');
-
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
 
-// Rutas
-app.get('/', (req, res, next) => {
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json());
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-
-});
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
-    // Si la base de datos no funciona error y no hago nada mas
+
     if (err) throw err;
 
-    console.log('Base de datos: puerto 3000: \x1b[32m%s\x1b[0m', ' online');
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
 });
+
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
 
 // Escuchar peticiones
 app.listen(3000, () => {
